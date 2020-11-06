91d3d6c8def8e3554b7881a0c79201300b95fbc8
Luqman Ferdjani <luqmanferdjani@gmail.com>
2020-11-06 19:40:18 +0100

    refactor: adding a command interface, handles command translation to assembly

---
+++ projects/07/project07/Command.java
@@ -0,0 +1,13 @@
+/**
+ * Interface of the commands type. Commands are lines of a VM file which contains instructions
+ * that can be translated into assembly
+ */
+public interface Command {
+
+  /**
+   * Method which translates a command into assembly 
+   * @return String[] an array of fun
+   */
+  public String[] translate();
+
+}
