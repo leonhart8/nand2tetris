e5040611476ffb088c037b7b9d29291ab3342d4a
Luqman Ferdjani <luqmanferdjani@gmail.com>
2020-11-06 19:40:42 +0100

    refactor: added a common interface to push commands

---
+++ projects/07/project07/CommandPush.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+
+/**
+ * Class used to define the interface of push commands
+ */
+public abstract class CommandPush {
+
+    // The name of the segment 
+    private String segment;
+    // The name of the reference to the segment
+    private String segmentRef;
+    // The index from which to push
+    private String index;
+
+    /**
+     * Translates a push command into assembly code
+     * @return LinkedList<String> a list of assembly commands to write
+     */
+    public LinkedList<String> translate(){
+        LinkedList<String> assembly = new LinkedList<String>();
+
+        // Build the commands that make up the entire push
+        String comment = buildComment();
+        LinkedList<String> getElementToPush = this.getElementToPush();
+        LinkedList<String> pushElement = this.pushElement();
+
+        // Link these commands together
+        assembly.add(comment);
+        for (String line : getElementToPush){
+            assembly.add(line);
+        }
+        for (String line : pushElement){
+            assembly.add(line);
+        }
+
+        return assembly;
+    } 
+
+    /**
+     * Writes the comment associated with the command
+     * @return String, the comment associated with the command
+     */
+    public String buildComment(){
+        return "// push " + this.segment + " " + this.index;
+    }
+
+    /**
+     * Writes the assembly code of a push where an element is taken from a specific
+     * adress to add to the stack
+     * @return LinkedList<String> the sequence of commands generated
+     */
+    public abstract LinkedList<String> getElementToPush();
+
+    /**
+     * Writes the assembly code necessary to add an element stored in the D
+     * register to the top of the stack and increments it
+     */
+    public abstract LinkedList<String> pushElement();
+
+}
