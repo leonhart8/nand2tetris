e17cb184070ecad1647653742543806741d15b7d
Luqman Ferdjani <luqmanferdjani@gmail.com>
2020-11-07 15:52:45 +0100

    feat: CommandFactory now generates instances of push and pop commands

--- projects/07/project07/CommandFactory.java
+++ projects/07/project07/CommandFactory.java
@@ -4,12 +4,58 @@
  */
 public class CommandFactory {
 
-    /* /**
+    /**
      * Builds the appropriate command instance given a line in the .vm file
-     * @param command String, the vm command
+     * @param command String[], the tokenized vm command
+     * @param filenameWithoutExtension the name of the file being written into without its extension
      * @return Command, the command associated to the input
-    
-    public Command buildCommand(String[] tokens){
-    } */
+     */
+    public Command buildCommand(String[] tokens, String filenameWithoutExtension) throws IllegalArgumentException {
+        if (tokens.length == 3){
+            switch (tokens[0]) {
+                case "push":
+                    switch (tokens[1]) {
+                        case "local":
+                            return new CommandPushLocal(tokens[2]);
+                        case "argument":
+                            return new CommandPushArgument(tokens[2]);
+                        case "this":
+                            return new CommandPushThis(tokens[2]);
+                        case "that":
+                            return new CommandPushThat(tokens[2]);
+                        case "constant":
+                            return new CommandPushConstant(tokens[2]);
+                        case "static":
+                            return new CommandPushStatic(tokens[2], filenameWithoutExtension);
+                        case "temp":
+                            return new CommandPushTemp(tokens[2]);
+                        default:
+                            throw new IllegalArgumentException("Memory segment " + tokens[1] + "not handled");
+                    }
+                case "pop":
+                    switch (tokens[1]) {
+                        case "local":
+                            return new CommandPopLocal(tokens[2]);
+                        case "argument":
+                            return new CommandPopArgument(tokens[2]);
+                        case "this":
+                            return new CommandPopThis(tokens[2]);
+                        case "that":
+                            return new CommandPopThat(tokens[2]);
+                        case "static":
+                            return new CommandPopStatic(tokens[2], filenameWithoutExtension);
+                        case "temp":
+                            return new CommandPopTemp(tokens[2]);
+                        default:
+                            throw new IllegalArgumentException("Memory segment " + tokens[1] + "not handled");
+                    }
+                default:
+                    throw new IllegalArgumentException("Command type " + tokens[0] + " not handled by translator");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("VM command length does not correspond to any known command");
+        }
+    } 
 
 }
